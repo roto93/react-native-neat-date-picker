b403b8b7dfc93434d1c65cbb4a494b1a3451665a
arkanos <rdeigramos@gmail.com>
2022-06-01 02:40:25 +0200

    Added different languages for i18n

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,65 +1,25 @@
-export const getMonthInChinese = (num) => {
-    if (typeof num !== 'number') return
-    if (num < 0 | num >= 12) return
-    switch (num) {
-        case 0:
-            return '一月'
-        case 1:
-            return '二月'
-        case 2:
-            return '三月'
-        case 3:
-            return '四月'
-        case 4:
-            return '五月'
-        case 5:
-            return '六月'
-        case 6:
-            return '七月'
-        case 7:
-            return '八月'
-        case 8:
-            return '九月'
-        case 9:
-            return '十月'
-        case 10:
-            return '十一月'
-        case 11:
-            return '十二月'
-        default:
-            return ''
-    }
-}
+import cn from './i18n/cn.json';
+import en from './i18n/en.json';
+import de from './i18n/de.json';
+import es from './i18n/es.json';
+import fr from './i18n/fr.json';
+import pt from './i18n/pt.json';
 
-export const getMonthInEnglish = (num) => {
-    if (typeof num !== 'number') return
-    if (num < 0 | num >= 12) return
-    switch (num) {
-        case 0:
-            return 'Jan'
-        case 1:
-            return 'Feb'
-        case 2:
-            return 'Mar'
-        case 3:
-            return 'Apr'
-        case 4:
-            return 'May'
-        case 5:
-            return 'Jun'
-        case 6:
-            return 'Jul'
-        case 7:
-            return 'Aug'
-        case 8:
-            return 'Sep'
-        case 9:
-            return 'Oct'
-        case 10:
-            return 'Nov'
-        case 11:
-            return 'Dec'
+export const getTranslation = (language) => {
+    switch (language){
+        case "en":
+            return en
+        case "cn":
+            return cn
+        case "es":
+            return es
+        case "de":
+            return de
+        case "pt":
+            return pt
+        case "fr":
+            return fr
         default:
-            return ''
+            return en
     }
 }
